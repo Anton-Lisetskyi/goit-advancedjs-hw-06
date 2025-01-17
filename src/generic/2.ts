374be071b2e5f198c2a12e79345cb815ce6c7ef5
Anton Lisetskyi <addbox88@gmail.com>
2025-01-17 17:08:51 +0200

    fixed

--- src/generic/2.ts
+++ src/generic/2.ts
@@ -11,16 +11,14 @@ type AllType = {
   weight: number;
 };
 
-function compare<T extends Pick<AllType, keyof AllType>>(
-  top: Pick<AllType, "name" | "color">,
-  bottom: Pick<AllType, "position" | "weight">
+function compare<T extends keyof AllType, U extends keyof AllType>(
+  top: Pick<AllType, T>,
+  bottom: Pick<AllType, U>
 ): AllType {
   return {
-    name: top.name,
-    color: top.color,
-    position: bottom.position,
-    weight: bottom.weight,
-  };
+    ...top,
+    ...bottom,
+  } as AllType;
 }
 
 const top = {
