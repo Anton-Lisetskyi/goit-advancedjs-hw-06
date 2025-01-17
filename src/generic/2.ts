9cc301dad7655cf057244ce992bc849cc902353e
Anton Lisetskyi <addbox88@gmail.com>
2025-01-17 17:21:32 +0200

    fixed

--- src/generic/2.ts
+++ src/generic/2.ts
@@ -8,17 +8,19 @@ type AllType = {
   name: string;
   position: number;
   color: string;
-  weight: number;
-};
+  weight: number
+}
 
-function compare<T extends keyof AllType, U extends keyof AllType>(
-  top: Pick<AllType, T>,
-  bottom: Pick<AllType, U>
+function compare<T extends Pick<AllType, 'name' | 'color'>, U extends Pick<AllType, 'position' | 'weight'>>(
+  top: T,
+  bottom: U
 ): AllType {
   return {
-    ...top,
-    ...bottom,
-  } as AllType;
+    name: top.name,
+    color: top.color,
+    position: bottom.position,
+    weight: bottom.weight,
+  }
 }
 
 const top = {
